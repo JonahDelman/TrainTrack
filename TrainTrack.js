747953c48cd3abdc04f016a5c556e57b8406c34a
Jonah Delman <jonahdelman@Jonahs-MacBook-Pro.local>
2025-05-05 20:15:38 -0400

    sorted table, added more readable data format, created data verification function

--- TrainTrack.js
+++ TrainTrack.js
@@ -20,9 +20,37 @@ router.get("/viewTrains", (request, response) => {
   });
 });
 
+
+async function verifyForm(){
+  const target = Number(document.querySelector("#train").value);
+  let arr = await getTrainData();
+  if (!Number.isNaN(target) && arr.some(elem => elem.trainNum === target)){
+    return window.confirm(`Are you sure you want to submit your ticket reservation?\n
+      Route Name: ${train.routeName}\n
+      Number: ${train.trainNum}\n
+      Origin: ${train.origName}\n
+      Destination: ${train.destName}\n
+      Departure Time: ${train.departure}`);
+  } else {
+    alert("Train Invalid");
+    return false;
+  }
+}
+
+
 async function displayTrains() {
   let table =
-    "<table border = '1'><tr><th>Route Name</th><th>Train Number</th><th>Origin</th><th>Destination</th><th>Scheduled Departure Time From NCR</th></tr>";
+    "<table border = '1'><tr><th>Route Name</th><th>Train Number</th><th>Origin</th><th>Destination</th><th>Scheduled Departure Time From New Carrollton</th></tr>";
+  let trainData = await getTrainData();
+  trainData.sort((train1, train2) => new Date(train1.departure) - new Date(train2.departure));
+  trainData.forEach(train => table += `<tr><td>${train.routeName}</td><td>${train.trainNum}</td><td>${train.origName}</td><td>${train.destName}
+    </td><td>${train.departure}</td></tr>`);
+    return table;
+}
+
+
+async function getTrainData(){
+  const arr = new Array();
   let station = await amtrak.fetchStation("NCR");
   for (let i = 0; i < station.NCR.trains.length; i++) {
     const id = station.NCR.trains[i];
@@ -31,14 +59,15 @@ async function displayTrains() {
     const train = trains[0][0];
     const departure = new Date(
       train.stations.find((station) => station.name === "New Carrollton").schDep
-    ).toString();
-    let currTime = new Date().toString();
+    );
+    let currTime = new Date();
     if (departure > currTime) {
-      table += `<tr><td>${train.routeName}</td><td>${train.trainNum}</td><td>${train.origName}</td><td>${train.destName}
-          </td><td>${departure}</td></tr>`;
+      train.departure = departure.toLocaleString();
+      arr.push(train);
     }
   }
-  return table;
+
+  return arr;
 }
 
 module.exports = router;

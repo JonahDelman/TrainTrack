7e97b1ec3a5c8ebeabb14fc1fb888ecd443f454e
Jonah Delman <jonah.delman@gmail.com>
2025-05-07 10:41:44 -0400

    various updates

--- TrainTrack.js
+++ TrainTrack.js
@@ -46,54 +46,102 @@ router.get("/adminTrain", (request, response) => {
   });
 });
 
-
 router.use(bodyParser.urlencoded({ extended: false }));
 router.post("/confirmation", (request, response) => {
-  verifyTrain(request.body.train).then(valid => {
-    if (valid){
-    const reservation = {
-      name: request.body.firstName,
-      email: request.body.email,
-      passengerCount: request.body.passengerCount,
-      train: request.body.train,
-      luggageCount: request.body.luggageCount,
-      description: request.body.description
-    };
-    insert(reservation);
-    response.render("confirmation", reservation);
-    } else {
-      const reservation = {
-        name: "NONE",
-        email: "NONE",
-        passengerCount: "NONE",
-        train: "Invalid Train Selected. Please Try Booking Again",
-        luggageCount: "NONE",
-        description: "NONE"
-      };
-      response.render("confirmation", reservation);
-    }
-  }).catch(error => console.error(error));
+  verifyTrain(request.body.train)
+    .then((valid) => {
+      if (valid) {
+        const reservation = {
+          name: request.body.firstName,
+          email: request.body.email,
+          passengerCount: request.body.passengerCount,
+          train: request.body.train,
+          luggageCount: request.body.luggageCount,
+          description: request.body.description,
+        };
+        insert(reservation);
+        response.render("confirmation", reservation);
+      } else {
+        const reservation = {
+          name: "NONE",
+          email: "NONE",
+          passengerCount: "NONE",
+          train: "Invalid Train Selected. Please Try Booking Again",
+          luggageCount: "NONE",
+          description: "NONE",
+        };
+        response.render("confirmation", reservation);
+      }
+    })
+    .catch((error) => console.error(error));
 });
 
-async function verifyTrain(target){
-  let arr = await getTrainData();
-  return !Number.isNaN(target) && arr.some(elem => elem.trainNum === target);
-}
+router.get("/adminPassenger", (request, response) => {
+  response.render("adminPassenger", null);
+});
 
+router.post("/processAdminPassengers", async (request, response) => {
+  if (await verifyTrain(request.body.train)) {
+    const arr = await arrayOfPassengers(request.body.train);
+    let table = "";
+    for (let i = 0; i < arr.length; i++) {
+      let passenger = arr[i];
+      table += `<tr><td>${passenger.name}</td><td>${passenger.passengerCount}</td><td>${passenger.luggageCount}</td><td>${passenger.description}</td></tr>`;
+    }
+    table =
+      table === ""
+        ? "Valid Train, But No Passengers Found"
+        : `<table border = '1'><tr><th>Name</th><th>Passenger Count</th><th>Luggage Count</th><th>Luggage Description</th></tr>${table}</table>`;
+    const variable = { table: table };
+    response.render("processAdminPassengers", variable);
+  } else {
+    const variable = { table: "<strong>Invalid Train Number</strong>" };
+    response.render("processAdminPassengers", variable);
+  }
+});
+
+router.get("/adminCancel", (request, response) => {
+  response.render("adminCancel", null);
+});
+
+router.post("/cancellation", async (request, response) => {
+  if (await verifyTrain(request.body.train)) {
+    try {
+      await client.connect();
+      const database = client.db(databaseName);
+      const collection = database.collection(collectionName);
+      let filter = { train: request.body.train };
+      let result = await collection.deleteMany(filter);
+      const variable = {numDeleted : "Train Successfully Cancelled, " + result.deletedCount + " Passengers Removed", trainNum : request.body.train};
+      response.render("cancellation", variable);
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+    const variable = { numDeleted : "Train Not Found, Could Not Complete Task" , trainNum : request.body.train};
+    response.render("cancellation", variable);
+  }
+});
+
+async function verifyTrain(target) {
+  let arr = await getTrainData();
+  return !Number.isNaN(target) && arr.some((elem) => elem.trainNum === target);
+}
 
 async function displayTrains() {
   let table =
     "<table border = '1'><tr><th>Route Name</th><th>Train Number</th><th>Origin</th><th>Destination</th><th>Scheduled Departure Time From New Carrollton</th></tr>";
   let trainData = await getTrainData();
-  trainData.sort((train1, train2) => DateTime.fromFormat(train1.departure, DateTime.DATETIME_MED) -
-  DateTime.fromFormat(train2.departure, DateTime.DATETIME_MED));
-  trainData.forEach(train => table += `<tr><td>${train.routeName}</td><td>${train.trainNum}</td><td>${train.origName}</td><td>${train.destName}
-    </td><td>${train.departure}</td></tr>`);
-    return table;
+  trainData.forEach(
+    (train) =>
+      (table += `<tr><td>${train.routeName}</td><td>${train.trainNum}</td><td>${train.origName}</td><td>${train.destName}
+    </td><td>${train.departure}</td></tr>`)
+  );
+  table += "</table>";
+  return table;
 }
 
-
-async function getTrainData(){
+async function getTrainData() {
   const arr = new Array();
   let station = await amtrak.fetchStation("NCR");
   for (let i = 0; i < station.NCR.trains.length; i++) {
@@ -102,13 +150,20 @@ async function getTrainData(){
     const trains = Object.values(trainData);
     const train = trains[0][0];
     const departure = DateTime.fromISO(
-      train.stations.find((station) => station.name === "New Carrollton").schDep
-    , { zone: 'America/New_York' });
-    let currTime = DateTime.now().setZone('America/New_York');
+      train.stations.find((station) => station.name === "New Carrollton")
+        .schDep,
+      { zone: "America/New_York" }
+    );
+    let currTime = DateTime.now().setZone("America/New_York");
     if (departure > currTime) {
-      train.departure = departure.setZone('America/New_York').toLocaleString(DateTime.DATETIME_MED);
+      train.departure = departure.setZone("America/New_York");
       arr.push(train);
     }
+  }
+  arr.sort((train1, train2) => train1.departure - train2.departure);
+  for (let i = 0; i < arr.length; i++) {
+    let train = arr[i];
+    train.departure = train.departure.toLocaleString(DateTime.DATETIME_MED);
   }
 
   return arr;
@@ -118,25 +173,34 @@ async function displayAdminTrains() {
   let table =
     "<table border = '1'><tr><th>Route Name</th><th>Train Number</th><th>Origin</th><th>Destination</th><th>Scheduled Departure Time From New Carrollton</th><th>Passenger Count</th><th>Luggage Count</th></tr>";
   let trainData = await getTrainData();
-  trainData.sort((train1, train2) => new Date(train1.departure) - new Date(train2.departure));
-  await client.connect();
-  const database = client.db(databaseName);
-  const collection = database.collection(collectionName);
-  for (let i = 0; i < trainData.length; i++){
+  for (let i = 0; i < trainData.length; i++) {
     let train = trainData[i];
-    let filter = { train: train.trainNum };
-    const cursor = await collection.find(filter);
-    let arr = await cursor.toArray();
-    let pax = 0, luggage = 0;
-    for(j = 0; j < arr.length; j++){
+    let arr = await arrayOfPassengers(train.trainNum);
+    let pax = 0,
+      luggage = 0;
+    for (j = 0; j < arr.length; j++) {
       pax += Number(arr[j].passengerCount);
       luggage += Number(arr[j].luggageCount);
     }
     table += `<tr><td>${train.routeName}</td><td>${train.trainNum}</td><td>${train.origName}</td><td>${train.destName}
     </td><td>${train.departure}</td><td>${pax}</td><td>${luggage}</td></tr>`;
   }
-    return table;
+  table += "</table>";
+  return table;
 }
 
+async function arrayOfPassengers(trainNum) {
+  try {
+    await client.connect();
+    const database = client.db(databaseName);
+    const collection = database.collection(collectionName);
+    let filter = { train: trainNum };
+    const cursor = await collection.find(filter);
+    let arr = await cursor.toArray();
+    return arr;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 module.exports = router;

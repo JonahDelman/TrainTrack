4af36bb7cfc910dbf38d93fffbe90fc61ce4a4f4
ifuller140 <ianfuller140@gmail.com>
2025-05-08 15:01:58 -0400

    Added min and max for Passengers and added portNumber selection

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const portNumber = 5001;
-const trainTrack = require("./TrainTrack");
+const portNumber = process.argv[2] || 5001;const trainTrack = require("./TrainTrack");
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "templates"));
